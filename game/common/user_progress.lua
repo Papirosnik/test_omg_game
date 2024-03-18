625514a60387967a3e3308739d2ebf043bb53357
Slava K <svyatoslav.kondratiuk@playgendary.com>
2024-03-18 17:15:08 +0300

    send a message for gui updating

---
+++ game/common/user_progress.lua
@@ -0,0 +1,19 @@
+local user_progress = {
+    MSG_PROGRESS_CHANGED = hash("user_progress_changed"),
+    data = {
+        current_level = 1,
+        level_progress = {},
+        level_bonus_words = {},
+        total_bonus_words = 0
+    }
+}
+
+function user_progress.update_info(self)
+    msg.post("/gameplay/gameplay_gui_holder#gameplay_gui", self.MSG_PROGRESS_CHANGED, self.data)
+end
+
+function user_progress.load(self)
+    self:update_info()
+end
+
+return user_progress
