28a032783253cda63b7df5d590ef67d72952fb78
slava k <ipapirosnik@gmail.com>
2024-03-28 23:10:23 +0300

    get rid of hashes in word set

--- game/game/words_storage.lua
+++ game/game/words_storage.lua
@@ -3,7 +3,7 @@ local messages = require "game.common.messages"
 
 
 local words_storage = {
-    hash_set = {}
+    words = {}
 }
 
 
@@ -11,7 +11,7 @@ function words_storage.init(self)
     local data, error = sys.load_resource(const.FILENAME_WORDS)
     if data then
         for word in data:gmatch("[^\r\n]+") do
-            self.hash_set[hash(word)] = true
+            self.words[word] = true
         end
     else
         print(const.TXT_ERROR .. error .. const_TXT_BONUS_NOT_AVAILABLE)
@@ -20,7 +20,7 @@ end
 
 
 function words_storage.word_exists(self, word)
-    return self.hash_set[hash(word:lower())]
+    return self.words[word:lower()]
 end
 
 
